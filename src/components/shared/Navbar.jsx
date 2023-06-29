5cbd7c2f618ffa8e7cfc174886d901b466fb0631
Antonino Alampi <Alampiantonino99@gmail.com>
2023-06-29 11:31:14 +0200

    Removed bg-trasparent

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
             >
               <Link
                 className={[
-                  "px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline",
+                  "px-4 py-2 mt-2 text-sm font-semibold rounded-lg  dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline",
                   location.pathname == "/" ? "bg-gray-200" : "",
                 ].join(" ")}
                 to="/"
@@ -58,7 +58,7 @@ const Navbar = () => {
               </Link>
               <Link
                 className={[
-                  "px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline",
+                  "px-4 py-2 mt-2 text-sm font-semibold rounded-lg dark-mode:dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline",
                   location.pathname == "/login" ? "bg-gray-200" : "",
                 ].join(" ")}
                 to="/login"
